33cba5e65b1256788753fd348a55a927cab2122e
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-09 13:55:19 -0600

    prep for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items')
 
@@ -22,6 +23,16 @@ mongoose
 
 //use rout4es
 app.use('/api/items', items)
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.use.length('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
